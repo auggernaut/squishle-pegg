c51a2174ad4bd26a9afefef6532d0a9970978400
Auggernaut <augustin@datacosmos.com>
2014-08-13 17:42:50 -0700

    move categories to array on Card. change strings to pointers

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var squishle_pegg = require('./lib/squishle-pegg')
-  , options = 
-  			{ 
-  				feed_url: "http://squishle.meteor.com/json",
-				app_id: "sMSeqS1EP23z0vo3TgZKd38MBiP9qzrvnv0OHMk8",
-				master_key: "C8fABMIWRZ7qZeXakTmyifANFniJXpQXHoK5QOBu"
-			};
+  , options =
+  {
+    feed_url: "http://squishle.meteor.com/json",
+    app_id: "zogf8qxK4ULBBRBn2EhYWwddyUczTDks9w56mNsr",
+    master_key: "3ykoKuRAKk7zEBs4xejIfUqDmIxpOtaI5wyMH10R"
+  };
 
-squishle_pegg.importFeeds(options, function(err, res){
-	if(err) {
-		console.log("Error ocurred!"+err);
-		return;
-	}
-	if(res) 
-		console.log("Importing done successfully!");
-	else
-		console.log("Feeds imported already.");
+squishle_pegg.importFeeds(options, function (err, res) {
+  if (err) {
+    console.log("Error ocurred!" + err);
+    return;
+  }
+  if (res)
+    console.log("Importing done successfully!" + res);
+  else
+    console.log("Feeds imported already.");
 });
